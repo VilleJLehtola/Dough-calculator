5df3bc17fc4540dc8d0d72520bdef36c34b09147
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-24 10:22:26 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Taikinakalkulaattori</h1>
+      <p>Tervetuloa! Tämä on perusversio.</p>
+    </div>
+  );
+};
+
+export default App;
