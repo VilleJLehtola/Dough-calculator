48da4ff045bca076d08a2c82ef24203ca0451137
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-24 23:27:29 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://btozmkrowcrjzvxxhlbn.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0b3pta3Jvd2Nyanp2eHhobGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNTUyNTQsImV4cCI6MjA2ODkzMTI1NH0.b9d4e-ezcr486VRegrcl2CEv3uk4PJNLcwwSdq7mU98'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
