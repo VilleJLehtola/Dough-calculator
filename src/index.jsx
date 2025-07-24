3cee01104c983759ca2320be58909f6b6e7f5246
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-24 12:13:40 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './styles.css'; // or your CSS file
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><App /></React.StrictMode>
 );
