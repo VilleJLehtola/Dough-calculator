00a1d530bc1db31df62b22314924cb50684c1460
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-24 22:33:54 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 import "./index.css";
 
 const supabase = createClient(
-  "https://<your-supabase-url>.supabase.co",
-  "<your-anon-key>"
+  "https://btozmkrowcrjzvxxhlbn.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0b3pta3Jvd2Nyanp2eHhobGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzNTUyNTQsImV4cCI6MjA2ODkzMTI1NH0.b9d4e-ezcr486VRegrcl2CEv3uk4PJNLcwwSdq7mU98"
 );
 
 export default function App() {
