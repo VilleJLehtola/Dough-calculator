f0d54f678eea02763d9fc3c51201d09f6831ca67
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-24 10:31:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css'; // or './index.css' depending on your file
+import './styles.css'; // Adjust this path if needed
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
