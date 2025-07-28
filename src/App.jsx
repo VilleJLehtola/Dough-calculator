4c80c078e1124ace5876df0c3c632802bc18b5cf
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-07-28 14:50:34 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 import Header from './components/Header';
 import AuthForm from './components/AuthForm';
 import CalculatorForm from './components/CalculatorForm';
