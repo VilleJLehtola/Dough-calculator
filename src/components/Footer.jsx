edfd45bef417b96b9f9b7f4bfc57cf7b60e47fbd
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-06 21:33:31 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="w-full text-center text-sm py-4 text-gray-600 dark:text-gray-400">
+      © 2025 Taikinalaskin
+    </footer>
+  );
+}
