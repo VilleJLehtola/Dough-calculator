ffab710724d021119c449a0847259f7e7b4e53bb
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:27:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,8 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+  build: {
+    sourcemap: true,
   },
 });
+
