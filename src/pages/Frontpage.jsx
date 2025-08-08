f58e73c6dc574a644aaafb7dcb2ab8dfb8fc44b7
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 21:41:23 +0300

    Update Frontpage.jsx

--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -1,6 +1,6 @@
 // src/pages/FrontPage.jsx
 import { useEffect, useState } from 'react';
-import { supabase } from '@/supabaseClient';
+import supabase from '@/supabaseClient';
 import { useNavigate } from 'react-router-dom';
 
 export default function FrontPage() {
