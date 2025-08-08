920de6876d99c8c86415aa85bcda543d3f6fa73f
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:20:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-// App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Frontpage from "./components/Frontpage";
-import CalculatorPage from "./components/CalculatorForm";
-import FavoritesList from "./components/FavoritesList";
-import AdminRecipeEditor from "./components/AdminRecipeEditor";
-import RecipeViewPage from "./components/RecipeViewPage";
-import Layout from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
+import Frontpage from './components/Frontpage';
+import CalculatorForm from './components/CalculatorForm';
+import FavoritesList from './components/FavoritesList';
+import AdminRecipeEditor from './components/AdminRecipeEditor';
+import RecipeViewPage from './components/RecipeViewPage';
+import AuthForm from './components/AuthForm';
+import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Frontpage />} />
-          <Route path="/calculator" element={<CalculatorPage />} />
-          <Route path="/favorites" element={<FavoritesList />} />
-          <Route path="/admin" element={<AdminRecipeEditor />} />
-          <Route path="/recipe/:id" element={<RecipeViewPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Frontpage />} />
+        <Route path="/calculator" element={<CalculatorForm />} />
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="/admin" element={<AdminRecipeEditor />} />
+        <Route path="/recipe/:id" element={<RecipeViewPage />} />
+        <Route path="/login" element={<AuthForm />} />
+      </Routes>
+    </Layout>
   );
 }
