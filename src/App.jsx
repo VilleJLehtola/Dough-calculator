3edb7d9d9c956df50f67d122c785dc98a83b0ee8
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 21:43:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-// src/App.jsx
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Layout from './components/Layout'
-import Frontpage from './pages/Frontpage'
-import RecipeViewPage from './components/RecipeViewPage'
-import AuthForm from './components/AuthForm'
-
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Frontpage />} />
-        <Route path="login" element={<AuthForm />} />
-        <Route path="recipe/:id" element={<RecipeViewPage />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  )
+    <div>
+      <h1>Hello from App!</h1>
+    </div>
+  );
 }
+
+export default App;
