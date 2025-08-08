06bc85f074e56f2a7db62c843b84ed8291bfb551
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:25:01 +0300

    Update EditRecipePage.jsx

--- src/components/EditRecipePage.jsx
+++ src/components/EditRecipePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { supabase } from '@/supabaseClient';
+import supabase from '@/supabaseClient';
+
 import AdminRecipeEditor from './AdminRecipeEditor';
 
 export default function EditRecipePage({ user }) {
