68d03bb1fffd6e0dd1d7b5dcacb1ab797264f215
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:20:32 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
-// Layout.jsx
-import Sidebar from "./Sidebar";
-import Header from "./Header";
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
 
 export default function Layout({ children }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <Sidebar />
+      {/* Sidebar (toggle for mobile) */}
+      <Sidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
+
+      {/* Main content */}
       <div className="flex-1 flex flex-col">
-        <Header />
+        <Header toggleSidebar={() => setIsSidebarOpen((prev) => !prev)} />
         <main className="p-6">{children}</main>
       </div>
     </div>
