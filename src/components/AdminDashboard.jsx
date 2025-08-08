cd3d11f55e940dc34549cfd480ee99e6ee0874ab
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:24:43 +0300

    Update AdminDashboard.jsx

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
+import supabase from '@/supabaseClient';
+
 
 export default function AdminDashboard({ user }) {
   const [recipes, setRecipes] = useState([]);
