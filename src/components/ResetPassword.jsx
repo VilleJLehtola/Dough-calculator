d0a63f853adb3dab71ce43b3adc3a28ff562d9e6
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-08 20:25:27 +0300

    Update ResetPassword.jsx

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import supabase from '@/supabaseClient';
+
 
 export default function ResetPassword({ setActiveView }) {
   const [password, setPassword] = useState('');
