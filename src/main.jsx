23be95396c0a25eda5e977169425d1a620cc5a64
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-13 11:37:22 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import './i18n' // <-- ensure this is here
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
