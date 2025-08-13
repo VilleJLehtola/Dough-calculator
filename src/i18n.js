ff270364ceeebfb296dd65c5b1fb0baf404bddaa
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-13 11:40:34 +0300

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// Import translation resources
 import en from './common/en/common.json';
 import fi from './common/fi/common.json';
 import sv from './common/sv/common.json';
+
+const savedLang =
+  typeof window !== 'undefined'
+    ? window.localStorage?.getItem('lang')
+    : null;
 
 i18n
   .use(initReactI18next)
@@ -12,13 +16,14 @@ i18n
     resources: {
       en: { common: en },
       fi: { common: fi },
-      sv: { common: sv }
+      sv: { common: sv },
     },
-    lng: 'fi',            // default language
+    lng: savedLang || 'fi',
     fallbackLng: ['fi', 'en', 'sv'],
     ns: ['common'],
     defaultNS: 'common',
-    interpolation: { escapeValue: false }
+    interpolation: { escapeValue: false },
+    returnNull: false
   });
 
 export default i18n;
