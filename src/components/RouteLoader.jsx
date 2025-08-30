19b9cc49019e26b5ba664da1baf6072b589420e7
VilleJLehtola <ville.j.lehtola@gmail.com>
2025-08-31 02:12:31 +0300

    Create RouteLoader.jsx

---
+++ src/components/RouteLoader.jsx
@@ -0,0 +1,8 @@
+// src/components/RouteLoader.jsx
+export default function RouteLoader() {
+  return (
+    <div className="p-6 text-center text-sm text-gray-600 dark:text-gray-300">
+      Loading…
+    </div>
+  );
+}
