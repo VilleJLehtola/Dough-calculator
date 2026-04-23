e67a246f6913f35c2492e69a18fd47b78c003045
VilleJLehtola <ville.j.lehtola@gmail.com>
2026-04-23 14:35:47 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,12 @@
-// Layout.jsx
-import Sidebar from "./Sidebar";
-import Header from "./Header";
+import Sidebar from './Sidebar';
+import Header from './Header';
 
-export default function Layout({ children }) {
+export default function Layout({ children, user, onLogout }) {
   return (
     <div className="flex min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-      <Sidebar />
+      <Sidebar user={user} />
       <div className="flex-1 flex flex-col">
-        <Header />
+        <Header user={user} onLogout={onLogout} />
         <main className="p-6">{children}</main>
       </div>
     </div>
